40c17e229837c1df0202393f2e4650770b19e3fc
Parashara <parasharar@gmail.com>
2023-10-20 10:59:51 +0800

    made some cosmetic changes

--- Assignment 3 (Hidden Markov Models)/lab3.py
+++ Assignment 3 (Hidden Markov Models)/lab3.py
@@ -43,27 +43,6 @@ def initialize(n_states, x):
 
 """E-step"""
 
-#TODO.x manipulate this macha!
-def multivariate_normal_pdf(x, mu, cov):
-    k = mu.shape[0]
-    det_cov = np.linalg.det(cov)
-    inv_cov = np.linalg.inv(cov)
-    diff = x - mu
-
-    exponent = -0.5 * np.dot(np.dot(diff, inv_cov), diff)
-    coefficient = 1.0 / (np.sqrt((2 * np.pi) ** k * det_cov))
-
-    return coefficient * np.exp(exponent)
-
-def calculate_alphas_and_c(x_list, pi, phi, A):
-    '''
-    TODO.x most probably alphas should be of dimension (O,N)
-    '''
-    xs = np.array(x_list)
-    mu = phi["mu"]
-    sigma = phi["sigma"]
-    return
-
 
 def e_step(x_list, pi, A, phi):
     """ E-step: Compute posterior distribution of the latent variables,
@@ -91,21 +70,60 @@ def e_step(x_list, pi, A, phi):
     Be sure to use the scaling factor for numerical stability.
     """
     # 1. calculate scaled alpha
-    alphas, c_n = calculate_alphas_and_c(x_list, pi, phi, A)
-
-    # alphas_hat = calculate_scaled_alphas(alphas, c_n)
-
+    alphas_hat, c_n = Estep.calculate_scaled_alphas_and_c(x_list, pi, phi, A)
 
     # 2.  calculate scaled beta
-    # betas_hat = calculate_scaled_betas(x_list, pi, phi, A, c_n)
+    # betas_hat = Estep.calculate_scaled_betas(x_list, pi, phi, A, c_n)
 
     # 3. calculate gamma
-    # gamma_list = calculate_gamma(alphas_hat, betas_hat)
+    # gamma_list = Estep.calculate_gamma(alphas_hat, betas_hat)
 
     # 4 calculate xi_list
-    # xi_list = calculate_spring(alphas_hat, betas_hat, c_n, pi, phi, A)
+    # xi_list = Estep.calculate_spring(alphas_hat, betas_hat, c_n, pi, phi, A)
 
     return gamma_list, xi_list
+
+
+class Estep:
+    @staticmethod
+    def multivariate_normal_pdf(x, mu, cov):
+        k = mu.shape[0]
+        det_cov = np.linalg.det(cov)
+        inv_cov = np.linalg.inv(cov)
+        diff = x - mu
+
+        exponent = -0.5 * np.dot(np.dot(diff, inv_cov), diff)
+        coefficient = 1.0 / (np.sqrt((2 * np.pi) ** k * det_cov))
+
+        return coefficient * np.exp(exponent)
+
+    @staticmethod
+    def calculate_scaled_alphas_and_c(x_list, pi, phi, A):
+        xs = np.array(x_list) #Shape (O, N)
+        mu = phi["mu"] #Shape (K)
+        sigma = phi["sigma"] #Shape (K)
+
+        O = xs.shape[0]
+        N = xs.shape[1]
+        K = mu.shape[0]
+
+        alphas_hat = [] #should eventually be a shape of (O,N,K)
+        c = [] #should eventually be a shape of (O, N)
+
+        for o in range(O): # for each observation
+            # base case for this observation
+
+            # using this base case perform tabulation to find alpha tilde
+
+            # from alpha tilde find out the cn and alpha_hat(zn)
+
+            # store in the arrays
+
+
+            pass
+
+        #return the final scaled alphas and the constants
+        return np.array(alphas_hat), np.array(c)
 
 
 """M-step"""
@@ -140,16 +158,16 @@ def m_step(x_list, gamma_list, xi_list):
     """
 
     # 1. Finding new pi
-    pi = find_new_pi(gamma_list, pi)
+    pi = Mstep.find_new_pi(gamma_list, pi)
 
     # 2. Finding new mu
-    phi["mu"] = find_new_mu(gamma_list, x_list)
+    phi["mu"] = Mstep.find_new_mu(gamma_list, x_list)
 
     # 3. Finding new A
-    A = find_new_A(xi_list, A)
+    A = Mstep.find_new_A(xi_list, A)
 
     # 4. Finding new sigma
-    phi["sigma"] = find_new_sigma(gamma_list, x_list, phi["mu"])
+    phi["sigma"] = Mstep.find_new_sigma(gamma_list, x_list, phi["mu"])
 
     return pi, A, phi
 
@@ -157,112 +175,116 @@ def m_step(x_list, gamma_list, xi_list):
 """ Functions for calculating each new parameter"""
 
 
-def find_new_pi(gamma_list, pi):
-    # Getting (Obs, N, k) -> (Obs, k) ( picking it for the N = 1)
-    gamma_1s = np.vstack([gamma_obs[0] for gamma_obs in gamma_list])
-    # Numerator: Adding across all observations to get from (Obs, k) -> (k)
-    pi_num = np.sum(gamma_1s, axis=0)
-    # Denominator: Summing across the k to get the normalizer
-    pi_denom = np.sum(pi_num, axis=0)
-    # Computing pi
-    pi = pi_num / pi_denom
-    return pi
+class Mstep:
+    @staticmethod
+    def find_new_pi(gamma_list, pi):
+        # Getting (Obs, N, k) -> (Obs, k) ( picking it for the N = 1)
+        gamma_1s = np.vstack([gamma_obs[0] for gamma_obs in gamma_list])
+        # Numerator: Adding across all observations to get from (Obs, k) -> (k)
+        pi_num = np.sum(gamma_1s, axis=0)
+        # Denominator: Summing across the k to get the normalizer
+        pi_denom = np.sum(pi_num, axis=0)
+        # Computing pi
+        pi = pi_num / pi_denom
+        return pi
 
+    @staticmethod
+    def find_new_mu(gamma_list, x_list):
+        gammas = np.array(gamma_list)
+        # a. Finding Denominator
+        # Find sum across all time steps (Obs, N, k) -> (Obs, k)
+        mu_denom = np.sum(gammas, axis=1)
+        # Do this across all observations (Obs, k) -> (k)
+        mu_denom = np.sum(mu_denom, axis=0)
 
-def find_new_mu(gamma_list, x_list):
-    gammas = np.array(gamma_list)
-    # a. Finding Denominator
-    # Find sum across all time steps (Obs, N, k) -> (Obs, k)
-    mu_denom = np.sum(gammas, axis=1)
-    # Do this across all observations (Obs, k) -> (k)
-    mu_denom = np.sum(mu_denom, axis=0)
+        # b. Finding Numerator
+        xs = np.array(x_list)
+        # find the product with each x i.e. (Obs, N, k) x (Obs,N,1) -> (Obs, N, K) {the 1 is added using np.newaxis}
+        mu_num = gammas * xs[:, :, np.newaxis]
+        # Do summation over all timesteps to get from (Obs, N, K) -> (Obs, K)
+        mu_num = np.sum(mu_num, axis=1)
+        # Do summation over all observations to get from (Obs, K) -> (K)
+        mu_num = np.sum(mu_num, axis=0)
 
-    # b. Finding Numerator
-    xs = np.array(x_list)
-    # find the product with each x i.e. (Obs, N, k) x (Obs,N,1) -> (Obs, N, K) {the 1 is added using np.newaxis}
-    mu_num = gammas * xs[:, :, np.newaxis]
-    # Do summation over all timesteps to get from (Obs, N, K) -> (Obs, K)
-    mu_num = np.sum(mu_num, axis=1)
-    # Do summation over all observations to get from (Obs, K) -> (K)
-    mu_num = np.sum(mu_num, axis=0)
+        # c. finding mu
+        return mu_num / mu_denom
 
-    # c. finding mu
-    return mu_num / mu_denom
+    @staticmethod
+    def find_new_A(xi_list, A):
+        '''
+        Fill the Ajk values one by one
 
+        @param xi_list:
+        @param A:
+        @return:
+        '''
 
-def find_new_A(xi_list, A):
-    '''
-    Fill the Ajk values one by one
+        xis = np.array(xi_list)
+        K = A.shape[0]
+        O = xis.shape[0]
+        N = xis.shape[1] + 1
 
-    @param xi_list:
-    @param A:
-    @return:
-    '''
+        xi_n_sums_per_obs = []
+        for xi_obs in xis:  # Shape (Obs, N-1 , K, K)
+            # xi_obs is Shape (N-1 , K, K)
+            xi_n_sum_per_obs = np.zeros((K, K))
+            for xi_n in xi_obs:
+                # xi_n is Shape (K, K)
+                xi_n_sum_per_obs += xi_n
+            xi_n_sums_per_obs.append(xi_n_sum_per_obs)
+        xi_n_sums_per_obs = np.array(xi_n_sums_per_obs)
 
-    xis = np.array(xi_list)
-    K = A.shape[0]
-    O = xis.shape[0]
-    N = xis.shape[1] + 1
+        # Numerator
+        A_num = np.sum(xi_n_sums_per_obs, axis=0)
 
-    xi_n_sums_per_obs = []
-    for xi_obs in xis: # Shape (Obs, N-1 , K, K)
-        #xi_obs is Shape (N-1 , K, K)
-        xi_n_sum_per_obs = np.zeros((K,K))
-        for xi_n in xi_obs:
-            # xi_n is Shape (K, K)
-            xi_n_sum_per_obs += xi_n
-        xi_n_sums_per_obs.append(xi_n_sum_per_obs)
-    xi_n_sums_per_obs = np.array(xi_n_sums_per_obs)
+        # Denominator
+        A_denom = np.sum(A_num, axis=1)
+        A_denom = A_denom[:, np.newaxis]
 
-    #Numerator
-    A_num = np.sum(xi_n_sums_per_obs, axis=0)
+        # Compute A
+        A = A_num / A_denom
 
-    #Denominator
-    A_denom = np.sum(A_num, axis=1)
-    A_denom = A_denom[:, np.newaxis]
+        return A
 
-    #Compute A
-    A = A_num/A_denom
+    @staticmethod
+    def find_new_sigma(gamma_list, x_list, mu):
+        gammas = np.array(gamma_list)
+        xs = np.array(x_list)
 
-    return A
+        O = xs.shape[0]
+        N = xs.shape[1]
+        K = mu.shape[0]
 
+        covariance = np.zeros((K))
 
-def find_new_sigma(gamma_list, x_list, mu):
-    gammas = np.array(gamma_list)
-    xs = np.array(x_list)
+        # finding numerator
+        for k in range(K):
+            num_across_obs = 0
+            denom_across_obs = 0
+            for o in range(O):
+                num_across_N = 0
+                denom_across_N = 0
+                for n in range(N):
+                    granular_gamma = gammas[o, n, k]
+                    x_n = xs[o, n]
+                    mu_k = mu[k]
+                    diff = x_n - mu_k
+                    diff_transpose = diff.T
+                    granular_product = granular_gamma * diff * diff_transpose
+                    num_across_N += granular_product
+                    denom_across_N += granular_gamma
+                num_across_obs += num_across_N
+                denom_across_obs += denom_across_N
+            covariance[k] = num_across_obs / denom_across_obs
 
-    O = xs.shape[0]
-    N = xs.shape[1]
-    K = mu.shape[0]
+        # Take sqrt from covariance to get the sigma (i.e. std deviation)
+        sigma = np.sqrt(covariance)
 
-    covariance = np.zeros((K))
+        return sigma
 
-    #finding numerator
-    for k in range(K):
-        num_across_obs = 0
-        denom_across_obs = 0
-        for o in range(O):
-            num_across_N = 0
-            denom_across_N = 0
-            for n in range(N):
-                granular_gamma = gammas[o,n,k]
-                x_n = xs[o, n]
-                mu_k = mu[k]
-                diff = x_n - mu_k
-                diff_transpose = diff.T
-                granular_product = granular_gamma * diff * diff_transpose
-                num_across_N += granular_product
-                denom_across_N += granular_gamma
-            num_across_obs += num_across_N
-            denom_across_obs += denom_across_N
-        covariance[k] = num_across_obs/denom_across_obs
-
-    #Take sqrt from covariance to get the sigma (i.e. std deviation)
-    sigma = np.sqrt(covariance)
-
-    return sigma
 
 """Putting them together"""
+
 
 def fit_hmm(x_list, n_states):
     """Fit HMM parameters to observed data using Baum-Welch algorithm
@@ -308,7 +330,6 @@ def fit_hmm(x_list, n_states):
 
 
 if __name__ == '__main__':
-
     # Create sample arrays G and d
     G = np.random.rand(200, 8, 3)  # G shape: (200, 8, 3)
     d = np.random.rand(200, 8, 3)  # d shape: (200, 8, 3)
@@ -329,4 +350,3 @@ if __name__ == '__main__':
     # Step 5: Sum this final array along the first axis (shape (3,))
     final_result = np.sum(summed_array, axis=0)  # final_result shape: (3,)
     print("Fuck off")
-
